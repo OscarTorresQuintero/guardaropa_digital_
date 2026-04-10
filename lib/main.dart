8f78bb8a60c07f8c7c56faa2e85235d3ed6979cd
Sara Rodriguez <s.rodriguez.aprendizsena@gmail.com>
2026-04-10 14:51:06 -0500

    chore: inicializar proyecto Flutter y main establecido

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/wardrobe_provider.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => WardrobeProvider(),
+      child: const StyleStackApp(),
+    ),
+  );
+}
+
+class StyleStackApp extends StatelessWidget {
+  const StyleStackApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'StyleStack',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.deepPurple,
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(centerTitle: true),
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: Colors.deepPurple,
+        brightness: Brightness.dark,
+        useMaterial3: true,
+      ),
+      themeMode: ThemeMode.system,
+      home: const HomeScreen(),
+    );
+  }
+}
